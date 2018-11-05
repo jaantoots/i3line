62b2e30b6dde31f325042270db895d28db2788ed
Jaan Toots <jaan@jaantoots.org>
2018-11-05 02:05:18 +0000

    Implement interval loop aligned to seconds

--- i3line.c
+++ i3line.c
@@ -17,34 +17,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <signal.h>
 #include <sys/select.h>
 
-int wait_read(const int secs) {
-    struct timeval timeout;
-    timeout.tv_sec = secs;
-    timeout.tv_usec = 0;
+static int cont = 1;
+static void handler(int signum) {
+    if (signum != SIGUSR1) cont = 0;
+}
+
+int wait_read(int (*fp)(const char *line), const struct timespec *timeout) {
+    /* wait upto timeout to run fp with optional line from stdin */
     fd_set rfds;
     FD_ZERO(&rfds);
     FD_SET(0, &rfds);
-    int r = select(1, &rfds, NULL, NULL, &timeout);
+    int r = pselect(1, &rfds, NULL, NULL, timeout, NULL);
     if (r < 0) {
         perror("select()");
-        return r;
+        return fp(NULL);
     }
-    if (r == 0) return 0;
+    /* no input */
+    if (r == 0) return fp(NULL);
     /* stdin should be ready */
-    if (FD_ISSET(0, &rfds)) printf("stdin ready\n");
     char *line = NULL;
     size_t n = 0;
-    if (getline(&line, &n, stdin) != -1) printf("got line: %s---\n", line);
+    int val = 0;
+    if (getline(&line, &n, stdin) != -1) val = fp(line);
     free(line);
+    return val;
+}
+
+int update(const char *line) {
+    /* update output */
+    if (line != NULL) return printf("got line: %s---\n", line);
+    return printf("meh\n");
+}
+
+int interval_loop(const int interval) {
+    /* update on input and aligned seconds */
+    struct timespec now, next, timeout;
+    if (clock_gettime(CLOCK_REALTIME, &next)) {
+        perror("clock_gettime()");
+        exit(EXIT_FAILURE);
+    }
+    next.tv_nsec = 0;
+    while (cont) {
+        if (clock_gettime(CLOCK_REALTIME, &now)) {
+            perror("clock_gettime()");
+            exit(EXIT_FAILURE);
+        }
+        /* ensure next is in the future */
+        if (now.tv_sec >= next.tv_sec && now.tv_nsec >= next.tv_nsec) {
+            next.tv_sec = now.tv_sec + interval;
+        }
+        timeout.tv_sec = next.tv_sec - now.tv_sec;
+        timeout.tv_nsec = next.tv_nsec - now.tv_nsec;
+        if (timeout.tv_nsec < 0) {
+            timeout.tv_sec -= 1;
+            timeout.tv_nsec += 1000000000;
+        }
+        /* wait and update */
+        char buff[100];
+        strftime(buff, sizeof buff, "%F %T", gmtime(&now.tv_sec));
+        printf("%s.%09ld\n", buff, now.tv_nsec);
+        wait_read(update, &timeout);
+    }
     return 0;
 }
 
 int main(int argc, char *argv[]) {
-    for (int i = 0; i < 8; ++i) {
-        printf("waiting\n");
-        wait_read(4);
-    }
-    return 0;
+    struct sigaction sa, oa;
+    /* use handler to end loop and exit on SIGHUP or SIGTERM */
+    sa.sa_handler = handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGHUP, NULL, &oa);
+    if (oa.sa_handler != SIG_IGN) sigaction(SIGHUP, &sa, NULL);
+    sigaction(SIGTERM, NULL, &oa);
+    if (oa.sa_handler != SIG_IGN) sigaction(SIGTERM, &sa, NULL);
+    /* ignore SIGUSR1 to only return from pselect */
+    sigaction(SIGUSR1, NULL, &oa);
+    if (oa.sa_handler != SIG_IGN) sigaction(SIGUSR1, &sa, NULL);
+
+    /* run loop */
+    const int interval = 4;
+    return interval_loop(interval);
 }
