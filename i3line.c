6314b05512fc77fe37967b548c1af419752a8644
Jaan Toots <jaan@jaantoots.org>
2018-11-05 16:11:54 +0000

    Move update to start of while loop

--- i3line.c
+++ i3line.c
@@ -21,73 +21,72 @@
 #include <signal.h>
 #include <sys/select.h>
 
+#define DEBUG
+
 static int cont = 1;
 static void handler(int signum) {
+    /* end loop and exit on SIGHUP or SIGTERM
+     * return from pselect on SIGUSR1 */
     if (signum != SIGUSR1) cont = 0;
-}
-
-int wait_read(int (*fp)(const char *line), const struct timespec *timeout) {
-    /* wait upto timeout to run fp with optional line from stdin */
-    fd_set rfds;
-    FD_ZERO(&rfds);
-    FD_SET(0, &rfds);
-    int r = pselect(1, &rfds, NULL, NULL, timeout, NULL);
-    if (r < 0) {
-        perror("select()");
-        return fp(NULL);
-    }
-    /* no input */
-    if (r == 0) return fp(NULL);
-    /* stdin should be ready */
-    char *line = NULL;
-    size_t n = 0;
-    int val = 0;
-    if (getline(&line, &n, stdin) != -1) val = fp(line);
-    free(line);
-    return val;
 }
 
 int update(const char *line) {
     /* update output */
-    if (line != NULL) return printf("got line: %s---\n", line);
+    if (line != NULL) return printf("got line: %s", line);
     return printf("meh\n");
 }
 
 int interval_loop(const int interval) {
-    /* update on input and aligned seconds */
+    /* update on input, signals, or aligned seconds */
     struct timespec now, next, timeout;
     if (clock_gettime(CLOCK_REALTIME, &next)) {
         perror("clock_gettime()");
         exit(EXIT_FAILURE);
     }
     next.tv_nsec = 0;
+    /* cont is set to zero on SIGHUP or SIGTERM */
     while (cont) {
+        update(NULL); // this is here to handle signals correctly
+wait:;
         if (clock_gettime(CLOCK_REALTIME, &now)) {
             perror("clock_gettime()");
             exit(EXIT_FAILURE);
         }
         /* ensure next is in the future */
-        if (now.tv_sec >= next.tv_sec && now.tv_nsec >= next.tv_nsec) {
+        if (now.tv_sec >= next.tv_sec && now.tv_nsec >= next.tv_nsec)
             next.tv_sec = now.tv_sec + interval;
-        }
         timeout.tv_sec = next.tv_sec - now.tv_sec;
         timeout.tv_nsec = next.tv_nsec - now.tv_nsec;
         if (timeout.tv_nsec < 0) {
             timeout.tv_sec -= 1;
             timeout.tv_nsec += 1000000000;
         }
-        /* wait and update */
+
+#ifdef DEBUG
         char buff[100];
         strftime(buff, sizeof buff, "%F %T", gmtime(&now.tv_sec));
-        printf("%s.%09ld\n", buff, now.tv_nsec);
-        wait_read(update, &timeout);
+        fprintf(stderr, "%s.%09ld\n", buff, now.tv_nsec);
+#endif /* DEBUG */
+
+        fd_set rfds;
+        FD_ZERO(&rfds);
+        FD_SET(0, &rfds);
+        /* wait upto timeout for a signal or stdin to be readable */
+        if (pselect(1, &rfds, NULL, NULL, &timeout, NULL) > 0) {
+            /* positive return value means stdin should be ready */
+            char *line = NULL;
+            size_t n = 0;
+            if (getline(&line, &n, stdin) != -1) update(line);
+            free(line);
+            goto wait; // skip the empty update
+        }
     }
     return 0;
 }
 
 int main(int argc, char *argv[]) {
+    /* specify signal handler to exit cleanly or trigger updates */
     struct sigaction sa, oa;
-    /* use handler to end loop and exit on SIGHUP or SIGTERM */
     sa.sa_handler = handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
@@ -95,11 +94,10 @@ int main(int argc, char *argv[]) {
     if (oa.sa_handler != SIG_IGN) sigaction(SIGHUP, &sa, NULL);
     sigaction(SIGTERM, NULL, &oa);
     if (oa.sa_handler != SIG_IGN) sigaction(SIGTERM, &sa, NULL);
-    /* ignore SIGUSR1 to only return from pselect */
     sigaction(SIGUSR1, NULL, &oa);
     if (oa.sa_handler != SIG_IGN) sigaction(SIGUSR1, &sa, NULL);
 
     /* run loop */
-    const int interval = 4;
+    const int interval = (argc > 1) ? atoi(argv[1]) : 4;
     return interval_loop(interval);
 }
